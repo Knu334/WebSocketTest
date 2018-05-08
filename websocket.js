13fa7c3994d818fa542e3dc4641e70f42a5d960c
Knu334 <who.is.fuu@gmail.com>
2018-05-08 22:55:02 +0900

    必要データ抽出

--- websocket.js
+++ websocket.js
@@ -2,6 +2,10 @@ var WebSocketClient = require('websocket').client;
 
 var client = new WebSocketClient();
 
+var sendMsg = function(connection) {
+	connection.sendUTF('2');
+}
+
 client.on('connectFailed', function(error) {
 	console.log('Connect Erroe: ' + error.toString());
 });
@@ -15,11 +19,27 @@ client.on('connect', function(connection) {
 		console.log('close');
 	});
 
+	var header = null;
 	connection.on('message', function(message) {
 		var strMsg = JSON.stringify(message.utf8Data);
-		console.log(strMsg.substring(strMsg.indexOf('{'), strMsg.lastIndexOf('}') + 1).replace(/\\+\"/g, '\"').replace(/\\+(u\w{4})/g, '\\$1'));
-		//console.log(objMsg.data.user_name);
+
+		try {
+			objMsg = JSON.parse(strMsg.substring(strMsg.indexOf('{'), strMsg.lastIndexOf('}') + 1).replace(/\\+\"/g, '\"').replace(/\\+(u\w{4})/g, '\\$1'));
+		} catch(e) {
+			objMsg = {};
+		}
+
+		if (header == null) {
+			header = objMsg;
+			interval = setInterval(function() {connection.sendUTF('2')}, header.pingInterval);
+			return;
+		}
+
+		if (objMsg.type == 0 && objMsg.data.stamp == null) {
+			console.log(objMsg.data.message);
+		}
 	});
 });
 
-client.connect('')
+client.connect('');
+
